b94dfcec4f6655c2133fd7ea21a398f0f3f3271b
MunaLombe <3223352545@qq.com>
2023-10-10 13:27:53 +0300

    init developer

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import router from './routes/routes';
 import Webhook from './services/Webhook';
 dotenv.config()
 
+const PORT = 5000;//process.env.PORT || 5000;
 const app = express().use(body_parser.json());
-app.listen(process.env.PORT, () => console.log("webhook/API is listening in port " + process.env.PORT));
+app.listen(PORT, () => console.log("webhook/API is listening in port " + PORT));
 
 // Whatsapp routes, Accepts POST requests at /webhook endpoint
 app.post("/webhook", Webhook.listenMessages);
